5f427f38b5dbb67d9c2fa1b2c4639a1e2df13ba0
RodrigoHernandez06 <160176017+RodrigoHernandez06@users.noreply.github.com>
2024-05-08 12:46:08 -0600

    Add files via upload

---
+++ DoWhile_NUMMENOR.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n=0, i=0;
+    
+    cout<<"Ingrese un numero mayor o igual a 2: ";
+    cin>>n;
+    
+    for (i=0; i<=n; i++){
+        cout<<i<<"\n";
+    }
+    return 0;
+}
